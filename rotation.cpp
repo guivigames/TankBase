120b13d8b9950fa38e38a888df3d4293c609503f
guivigames <games@guivi.one>
2021-09-15 20:53:40 +0100

    Shut a bullet when spacebar is pressed

--- rotation.cpp
+++ rotation.cpp
@@ -16,23 +16,40 @@ struct player
     sf::Sprite sprite;
 };
 
+struct bullet
+{
+    sf::Vector2f direction{ 0, 1};
+    float velocity{ 75};
+    sf::Vector2f position{ 100, 100};
+    sf::Sprite sprite;
+    float angle;
+};
+
 sf::Vector2f angle2direction(float angle)
 {
     sf::Vector2f _direction{ sin(-angle*M_PI/180), cos(-angle*M_PI/180)};
     return _direction;
 }
 
+bool isOut( bullet b)
+{
+    if(b.position.x <= 0 || b.position.y <= 0 || b.position.y >= 800 || b.position.x >= 1442)
+        return true;
+    return false;
+}
+
 int main()
 {   
     // Create the main window
-    sf::RenderWindow window(sf::VideoMode( 800, 600), "SFML window");
+    sf::RenderWindow window(sf::VideoMode( 1422, 800), "SFML window");
     printf("Window created\r\n");
     TextureManager::GetTexture("allSprites_default.png"); 
     printf("Sprite loaded\r\n");
     float _angle = 0.0;
     sf::Clock renderTimer;
-
+    sf::Clock _fpsClock;
     player tank_one;
+    std::vector<bullet> _bullets;
     tank_one.position = {100, 100};
     tank_one.sprite = TextureManager::GetSprite("allSprites_default.png", "tankBody_green_outline.png");
     tank_one.sprite.setPosition(tank_one.position);
@@ -49,44 +66,70 @@ int main()
             case sf::Event::Closed:
                 window.close();
                 break;
+            case sf::Event::KeyPressed: ///< Add a bullet
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+                {
+                    bullet shot;
+                    shot.sprite = TextureManager::GetSprite( "allSprites_default.png", "bulletGreen1_outline.png");
+                    shot.sprite.setOrigin(4, 7);
+                    shot.angle = _angle;
+                    shot.direction = tank_one.direction;
+                    shot.velocity = { 75};
+                    shot.position = tank_one.position;
+                    _bullets.push_back(shot);
+                }
+                break;
             }
         }
 
-        if (renderTimer.getElapsedTime().asMilliseconds() > 40)
+        sf::Time _fpsTimer = _fpsClock.restart();  ///< Frame rate controller.
+        float _fpsCoef =_fpsTimer.asSeconds();
+
+        renderTimer.restart(); ///< Restart timer;
+
+        /// Move the tank.
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) _angle -= (1 *_fpsCoef*50) ;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) _angle += (1*_fpsCoef*50);
+
+        float _velocity = 0;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) _velocity += (1.0*_fpsCoef*50);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) _velocity -= (1.0*_fpsCoef*50);
+
+        tank_one.direction = angle2direction(_angle);
+        tank_one.position = tank_one.position + (tank_one.direction * _velocity);
+
+        for (auto& shot : _bullets)
         {
-            renderTimer.restart(); ///< Restart timer;
-
-            /// Move the tank.
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) _angle -= 1;
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) _angle += 1;
-
-            float _velocity = 0;
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) _velocity += 1.0;
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) _velocity -= 1.0;
-
-            tank_one.direction = angle2direction(_angle);
-            tank_one.position = tank_one.position + (tank_one.direction * _velocity);
-
-            /// Render Game
-            // Clear screen
-            window.clear();
-            // Draw the sprite
-            window.draw(tank_one.sprite);
-            // Draw the string
-            //window.draw(text);
-            // Update the window
-            tank_one.sprite.setRotation(_angle);
-            tank_one.sprite.setPosition(tank_one.position);
-            //angle += 0.1;
-            //if(angle >= 360)
-            //    angle = 0.0;
-            // get the current mouse position in the window.
-            sf::Vector2i pixelPos = sf::Mouse::getPosition(window);
-            // convert it to world coordinates.
-            sf::Vector2f worldPos = window.mapPixelToCoords(pixelPos);
-
-            window.display();
+            shot.position = shot.position + (shot.direction * shot.velocity * _fpsCoef);
         }
+        std::remove_if(_bullets.begin(), _bullets.end(), isOut);
+
+        /// Render Game
+        // Clear screen
+        window.clear();
+        // Draw the sprite
+        tank_one.sprite.setRotation(_angle);
+        tank_one.sprite.setPosition(tank_one.position);
+        window.draw(tank_one.sprite);
+        // Draw the string
+        //window.draw(text);
+        // Update the window
+        
+        for (auto& shot : _bullets)
+        {
+            shot.sprite.setRotation(shot.angle);
+            shot.sprite.setPosition(shot.position);
+            window.draw(shot.sprite);
+        }
+        //angle += 0.1;
+        //if(angle >= 360)
+        //    angle = 0.0;
+        // get the current mouse position in the window.
+        sf::Vector2i pixelPos = sf::Mouse::getPosition(window);
+        // convert it to world coordinates.
+        sf::Vector2f worldPos = window.mapPixelToCoords(pixelPos);
+
+        window.display();
     }
     return EXIT_SUCCESS;
 }
